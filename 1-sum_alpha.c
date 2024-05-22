6bad7fd4850d82b13df9e447b64963da657d1b52
Festongithub <fkemboik@gmail.com>
2024-05-22 00:44:30 -0700

    Simple shell

---
+++ 1-sum_alpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * add_numbers - gives sum of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0
+ **/
+
+int add_numbers(int a, int b)
+{
+	int sum = a + b;
+
+	printf("sum is: %d", sum);
+}
